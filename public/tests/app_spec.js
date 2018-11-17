afc69818036ee8390236c7b73bb0b8d964ab08cf
Nakamura Kosuke <ncl0709@gmail.com>
2018-11-17 11:20:57 +0900

    Add hash view parameter test

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -8,4 +8,10 @@ describe('LearnJS', function() {
     learnjs.showView('');
     expect($('.view-container .landing-view').length).toEqual(1);
   });
+
+  it('passes the hash view parameter to the view function', function() {
+    spyOn(learnjs, 'problemView');  // arg1: object, arg2: func name
+    learnjs.showView('#problem-42');
+    expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  });
 });
