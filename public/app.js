d961402a31bc84922d954e0da7e3c7be31f3a6f0
Nakamura Kosuke <ncl0709@gmail.com>
2018-11-18 12:18:02 +0900

    Add fadeIn/Out animation

--- public/app.js
+++ public/app.js
@@ -33,9 +33,9 @@ learnjs.problemView = function(data) {
 
   function checkAnswerClick() {
     if (checkAnswer()) {
-      resultFlash.text('Correct!');
+      learnjs.flashElement(resultFlash, 'Correct!');
     } else {
-      resultFlash.text('Incorrect!');
+      learnjs.flashElement(resultFlash, 'Incorrect!');
     }
     return false;
   }
@@ -63,3 +63,10 @@ learnjs.appOnReady = function() {
   };
   learnjs.showView(window.location.hash);
 };
+
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  });
+}
