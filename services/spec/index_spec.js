4f5c298278ea41deaeb5a8ebe9537adfff5e3b98
Nakamura Kosuke <ncl0709@gmail.com>
2018-11-24 15:55:47 +0900

    Test popularAnswers()

--- services/spec/index_spec.js
+++ services/spec/index_spec.js
@@ -3,14 +3,30 @@ describe('lambda function', function() {
   var context;
 
   beforeEach(function() {
-    context = jasmine.createSpyObj('context', ['succeed']);
+    context = jasmine.createSpyObj('context', ['succeed', 'fail']);
+    index.dynamoDb = jasmine.createSpyObj('dynamo', ['scan']);
   });
 
-  describe('echo', function() {
-    it('returns a result', function() {
-      index.echo({}, context);
-      expected = ["Hello from the cloud! You sent {}"];
-      expect(context.succeed).toHaveBeenCalledWith(expected);
+  describe('popularAnswers', function() {
+    it('returns problems with the given problem number', function() {
+      index.popularAnswers({problemNumber: 42}, context);
+      expect(index.dynamoDb.scan).toHaveBeenCalledWith({
+        FilterExpression: "problemId = :problemId",
+        ExpressionAttributeValues: { ":problemId": 42 },
+        TableName: 'learnls'
+      }, jasmine.any(Function));
+    });
+
+    it ('group answers by minified code', function() {
+      index.popularAnswers({problemNumber: 1}, context);
+      index.dynamoDb.scan.calls.first().args[1](undefined, {Items: [
+        {answer: "true"},
+        {answer: "true"},
+        {answer: "true"},
+        {answer: "!false"},
+        {answer: "!false"},
+      ]});
+      expect(context.succeed).toHaveBeenCalledWith({"true": 3, "!false": 2});
     });
   });
 });
